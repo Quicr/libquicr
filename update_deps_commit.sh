be4b9bde6dd18e140796ec1f86cf930704822a5c
Suhas Nandakumar <snandaku@cisco.com>
2022-07-12 22:33:57 -0700

    update commit for pq as well

--- update_deps_commit.sh
+++ update_deps_commit.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
-#build last picotls master (for Travis)
 
 # Build at a known-good commit
-COMMIT_ID=7ff95968e783799236d1ad642c69a21e1e73e575
 echo "In update script"
+if [ -d "./contrib/picoquic" ]
+then
+  echo "changing commit for picoquic"
+  cd contrib/picoquic
+  git checkout "suhas"
+  cd ../..
+else
+  echo "NOT changing commit for picoquic" 
+fi
+
 if [ -d "./contrib/qproto" ]
 then
   echo "changing commit for qproto"
   cd contrib/qproto
-  git checkout -q "$COMMIT_ID"
+  git checkout "suhas"
   cd ../..
 else
   echo "NOT changing commit for qproto"
