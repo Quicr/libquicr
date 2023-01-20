8e4a0531f589156646fbacbe38243d6a889a9c57
suhasHere <snandaku@cisco.com>
2023-01-20 02:21:15 -0800

    server updates

--- include/quicr/quicr_server.h
+++ include/quicr/quicr_server.h
@@ -42,12 +42,12 @@ class ServerDelegate
    *           It is expected for the Relays to store the publisher state mapping
    *           the context, namespaces and other relation information.
    */
-  virtual void on_publish_intent(const QUICRContext& context_id,
-                                 const QUICRNamespace& quicr_name,
-                                 const std::string& origin_url,
-                                 bool use_reliable_transport,
-                                 const std::string& auth_token,
-                                 bytes&& payload) = 0;
+  virtual void onPublishIntent(const QUICRContext& context_id,
+                               const QUICRNamespace& quicr_name,
+                               const std::string& origin_url,
+                               bool use_reliable_transport,
+                               const std::string& auth_token,
+                               bytes&& payload) = 0;
 
   /*
    * @brief Reports arrival of fully assembled QUICR object under the name
@@ -74,11 +74,11 @@ class ServerDelegate
    *         callbacks will be called. The delegate implementation
    *         shall decide the right callback for their usage.
    */
-  virtual void on_published_object(const QUICRName& quicr_name,
-                                   uint8_t priority,
-                                   uint64_t best_before,
-                                   bool use_reliable_transport,
-                                   bytes&& data) = 0;
+  virtual void onPublisherObject(const QUICRName& quicr_name,
+                                 uint8_t priority,
+                                 uint64_t best_before,
+                                 bool use_reliable_transport,
+                                 bytes&& data) = 0;
 
   /*
    * @brief arrival of published QUICR object fragment under a Name
@@ -105,13 +105,13 @@ class ServerDelegate
    *         callbacks will be called. The delegate implementation
    *         shall decide the right callback for their usage.
    */
-  virtual void on_published_fragment(const QUICRName& quicr_name,
-                                     uint8_t priority,
-                                     uint64_t best_before,
-                                     bool use_reliable_transport,
-                                     const uint16_t fragment_number,
-                                     uint16_t num_total_fragments,
-                                     bytes&& data) = 0;
+  virtual void onPublishedFragment(const QUICRName& quicr_name,
+                                   uint8_t priority,
+                                   uint64_t best_before,
+                                   bool use_reliable_transport,
+                                   const uint16_t fragment_number,
+                                   uint16_t num_total_fragments,
+                                   bytes&& data) = 0;
 
   /*
    * @brief Report arrival of subscribe request for a QUICR Namespace
@@ -135,7 +135,7 @@ class ServerDelegate
    *          It is expected for the Relays to store the subscriber state mapping
    *          the subscribe context, namespaces and other relation information.
    */
-  virtual void on_subscribe(const QUICRContext& context_id,
+  virtual void onSubscribe(const QUICRContext& context_id,
                             const QUICRNamespace& quicr_namespace,
                             const SubscribeIntent subscribe_intent,
                             const std::string& origin_url,
@@ -146,15 +146,24 @@ class ServerDelegate
 
 class QuicRServer
 {
-
   /*
    * Start the  QUICR server at the port specified.
    *  @param delegate: Callback handlers for QUICR operations
    */
-  QuicRServer(const uint16_t port, ServerDelegate& delegate) explicit;
+  QuicRServer(ITransport&, ServerDelegate& delegate) explicit;
 
   // Transport APIs
   bool is_transport_ready();
+
+  /**
+   * @brief Run Server API event loop
+   *
+   * @details This method will open listening sockets and run an event loop
+   *    for callbacks.
+   *
+   * @returns true if error, false if no error
+   */
+  virtual bool run() = 0;
 
   /*
    * @brief Send publish intent response
@@ -166,9 +175,9 @@ class QuicRServer
    * @details Entities processing the Subscribe Request MUST validate the request 
    * @todo: Add payload with origin signed blob
    */ 
-  void publish_intent_response(const QUICRContext& context_id,
-                               const QUICRNamespace& quicr_namespace,
-                               const PublishIntentResult& result);
+  void publishIntentResponse(const QUICRContext& context_id,
+                             const QUICRNamespace& quicr_namespace,
+                             const PublishIntentResult& result);
 
   /*
    * @brief Send subscribe response 
@@ -180,9 +189,9 @@ class QuicRServer
    * @details Entities processing the Subscribe Request MUST validate the request 
    * @todo: Add payload with origin signed blob
    */
-  void subscribe_response(const QUICRContext& context_id,
-                          const QUICRNamespace& quicr_namespace,
-                          const SubscribeResult& result);
+  void subscribeResponse(const QUICRContext& context_id,
+                         const QUICRNamespace& quicr_namespace,
+                         const SubscribeResult& result);
 
   
   /*
@@ -196,9 +205,9 @@ class QuicRServer
    *           the stream or subscription timeout or other application
    *           reasons
    */
-  void subscription_ended(const QUICRContext& context_id,
-                          const QUICRNamespace& quicr_namespace,
-                          const SubscribeResult& result);
+  void subscriptionEnded(const QUICRContext& context_id,
+                         const QUICRNamespace& quicr_namespace,
+                         const SubscribeResult& result);
 
   /*
    * @brief Send a named QUICR media object
@@ -212,12 +221,12 @@ class QuicRServer
    * @param data                     : Opaque object payload
    * 
    */
-  void send_named_object(const QUICRContext& context_id,
-                         const QUICRName& quicr_name,
-                         uint8_t priority,
-                         uint64_t best_before,
-                         bool use_reliable_transport,
-                         bytes&& data);
+  void sendNamedObject(const QUICRContext& context_id,
+                       const QUICRName& quicr_name,
+                       uint8_t priority,
+                       uint64_t best_before,
+                       bool use_reliable_transport,
+                       bytes&& data);
 
    /*
     * @brief Send a named QUICR media object fragment
@@ -231,14 +240,14 @@ class QuicRServer
     * @param data                     : Opaque object fragment payload
     * 
    */
-  void send_named_fragment(const QUICRContext& context_id,
-                           const QUICRName& name,
-                           uint8_t priority,
-                           uint64_t best_before,
-                           bool use_reliable_transport,
-                           uint16_t fragment_number,
-                           uint16_t num_total_fragments,
-                           bytes&& data);
+  void sendNamedFragment(const QUICRContext& context_id,
+                         const QUICRName& name,
+                         uint8_t priority,
+                         uint64_t best_before,
+                         bool use_reliable_transport,
+                         uint16_t fragment_number,
+                         uint16_t num_total_fragments,
+                         bytes&& data);
 };
     
 } // namespace quicr
