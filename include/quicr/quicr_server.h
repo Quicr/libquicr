12ad5412c680319a730d0f11831a759af5bd5c54
suhasHere <snandaku@cisco.com>
2023-01-22 06:52:46 -0800

    address review feedback

--- include/quicr/quicr_server.h
+++ include/quicr/quicr_server.h
@@ -24,41 +24,37 @@ class ServerDelegate
    * @brief  Reports interest to publish under given
    * QuicrName. 
    * 
-   * @param context_id            : Context identifier that binds the namespace 
-   *                                to the publisher. Implementations must pass
-   *                                in this context identifer when sending
-   *                                response to the publish_intent.
    * @param namespace             : Identifies QUICR namespace
-   * @param origin_url            : Origin serving the QUICR Session    
-   * @param use_reliable_transport: Reliable or Unreliable transport 
-   * @param auth_token            : Auth Token to valiadate the Subscribe Request
-   * @parm payload                : Opaque payload to be forwarded to the Origin 
+   * @param origin_url            : Origin serving the QUICR Session
+   * @param use_reliable_transport: Reliable or Unreliable transport
+   * @param auth_token            : Auth Token to validate the Subscribe Request
+   * @parm e2e_token              : Opaque token to be forwarded to the Origin
    * 
    *  @details Entities processing the Publish Intent MUST validate the request 
-   *           against the token, verify if the Origin specified in the origin_url
-   *           is trusted and forward the request to the next hop Relay for that
+   *           against the auth_token, verify if the Origin specified
+   *           in the origin_url is trusted and forward the request to the
+   *           next hop Relay for that
    *           Origin or to the Origin (if it is the next hop) unless the entity 
    *           itself the Origin server.
    *           It is expected for the Relays to store the publisher state mapping
-   *           the context, namespaces and other relation information.
+   *           the namespaces and other relation information.
    */
-  virtual void onPublishIntent(const QUICRContext& context_id,
-                               const QUICRNamespace& quicr_name,
+  virtual void onPublishIntent(const QUICRNamespace& quicr_name,
                                const std::string& origin_url,
                                bool use_reliable_transport,
                                const std::string& auth_token,
-                               bytes&& payload) = 0;
+                               bytes&& e2e_token) = 0;
 
   /*
    * @brief Reports arrival of fully assembled QUICR object under the name
    *
    * @param quicr_name               : Identifies the QUICR Name for the object
-   * @param priority                 : Identifies the relative priority of the current object
-   * @param best_before              : TTL for the object to be useful for the application
+   * @param priority                 : Identifies the relative priority of the
+   *                                   current object
+   * @param expiry_age_ms            : Time hint for the object to be in cache
+   *                                   before being purged after reception
    * @param use_reliable_transport   : Indicates the preference for the object's
    *                                   transport, if forwarded.
-   * @param fragment_number          : Current Fragment Identifier
-   * @param num_total_fragments      : identifies current object's fragment count
    * @param data                     : Opaque payload of the fragment
    * 
    *  @details Entities implementing this API shall perform look up
@@ -66,7 +62,7 @@ class ServerDelegate
    *           objects.
    *  
    * @note: It is important that the implementations not perform 
-   *         compute intesive tasks in this callback, but rather
+   *         compute intensive tasks in this callback, but rather
    *         copy/move the needed information and hand back the control
    *         to the stack  
    * 
@@ -76,7 +72,7 @@ class ServerDelegate
    */
   virtual void onPublisherObject(const QUICRName& quicr_name,
                                  uint8_t priority,
-                                 uint64_t best_before,
+                                 uint16_t expiry_age_ms,
                                  bool use_reliable_transport,
                                  bytes&& data) = 0;
 
@@ -84,39 +80,37 @@ class ServerDelegate
    * @brief arrival of published QUICR object fragment under a Name
    *
    * @param quicr_name               : Identifies the QUICR Name for the object
-   * @param priority                 : Identifies the relative priority of the current object
-   * @param best_before              : TTL for the object to be useful for the application
+   * @param priority                 : Identifies the relative priority of the
+   *                                   current object
+   * @param expiry_age_ms            : Time hint for the object to be in cache
+*                                      before being purged after reception
    * @param use_reliable_transport   : Indicates the preference for the object's
    *                                   transport, if forwarded.
-   * @param fragment_number          : Current Fragment Identifier
-   * @param num_total_fragments      : identifies current object's fragment count
+   * @param offset                   : Current fragment offset
+   * @param is_last_fragment         : Indicates if the current fragment is the
+   *                                   last fragment
    * @param data                     : Opaque payload of the fragment
    * 
-   *  @details For cases where the latecny needs to kept low, Relays 
-   *           shall use the framgment API to store and forward objects
+   *  @details For cases where the latency needs to kept low, Relays
+   *           shall use the fragment API to store and forward objects
    *           as fragments. 
    * 
    *  @note: It is important that the implementations not perform 
-   *         compute intesive tasks in this callback, but rather
+   *         compute intensive tasks in this callback, but rather
    *         copy/move the needed information and hand back the control
-   *         to the stack  
-   * 
-   *  @note: Both the on_publish_object and on_publish_object_fragment
-   *         callbacks will be called. The delegate implementation
-   *         shall decide the right callback for their usage.
+   *         to the stack
    */
   virtual void onPublishedFragment(const QUICRName& quicr_name,
                                    uint8_t priority,
-                                   uint64_t best_before,
+                                   uint16_t expiry_age_ms,
                                    bool use_reliable_transport,
-                                   const uint16_t fragment_number,
-                                   uint16_t num_total_fragments,
+                                   const uint64_t& offset,
+                                   bool is_last_fragment,
                                    bytes&& data) {}
 
   /*
    * @brief Report arrival of subscribe request for a QUICR Namespace
    * 
-   * @param context_id            : Opaque context identifier for correlation.
    * @param namespace             : Identifies QUICR namespace
    * @param subscribe_intent      : Subscribe intent to determine the start point for 
    *                                 serving the mactched objects. The application
@@ -135,13 +129,12 @@ class ServerDelegate
    *          It is expected for the Relays to store the subscriber state mapping
    *          the subscribe context, namespaces and other relation information.
    */
-  virtual void onSubscribe(const QUICRContext& context_id,
-                            const QUICRNamespace& quicr_namespace,
-                            const SubscribeIntent subscribe_intent,
-                            const std::string& origin_url,
-                            bool use_reliable_transport,
-                            const std::string& auth_token,
-                            bytes&& data) {}
+  virtual void onSubscribe(const QUICRNamespace& quicr_namespace,
+                           const SubscribeIntent subscribe_intent,
+                           const std::string& origin_url,
+                           bool use_reliable_transport,
+                           const std::string& auth_token,
+                           bytes&& data) {}
 };
 
 class QuicRServer
@@ -168,36 +161,31 @@ class QuicRServer
   /*
    * @brief Send publish intent response
    * 
-   * @param context_id            : Opaque context identifier for correlation
    * @param quicr_namespace       : Identifies QUICR namespace
    * @param result                : Status of Publish Intetn 
    * 
    * @details Entities processing the Subscribe Request MUST validate the request 
    * @todo: Add payload with origin signed blob
    */ 
-  void publishIntentResponse(const QUICRContext& context_id,
-                             const QUICRNamespace& quicr_namespace,
+  void publishIntentResponse(const QUICRNamespace& quicr_namespace,
                              const PublishIntentResult& result);
 
   /*
    * @brief Send subscribe response 
    * 
-   * @param context_id            : Opaque context identifier for correlation
    * @param quicr_namespace       : Identifies QUICR namespace
    * @param result                : Status of Subscribe operation 
    * 
    * @details Entities processing the Subscribe Request MUST validate the request 
    * @todo: Add payload with origin signed blob
    */
-  void subscribeResponse(const QUICRContext& context_id,
-                         const QUICRNamespace& quicr_namespace,
+  void subscribeResponse(const QUICRNamespace& quicr_namespace,
                          const SubscribeResult& result);
 
   
   /*
    * @brief Indicates a given subscription is no longer valid
    * 
-   * @param context_id            : Opaque context identifier for correlation
    * @param quicr_namespace       : Identifies QUICR namespace
    * @param result                : Status of Subscribe operation 
    * 
@@ -205,14 +193,12 @@ class QuicRServer
    *           the stream or subscription timeout or other application
    *           reasons
    */
-  void subscriptionEnded(const QUICRContext& context_id,
-                         const QUICRNamespace& quicr_namespace,
+  void subscriptionEnded(const QUICRNamespace& quicr_namespace,
                          const SubscribeResult& result);
 
   /*
    * @brief Send a named QUICR media object
    *
-   * @param context_id               : Opaque context identifier for correlation
    * @param quicr_name               : Identifies the QUICR Name for the object
    * @param priority                 : Identifies the relative priority of the current object
    * @param best_before              : TTL for the object to be useful for the application
@@ -221,8 +207,7 @@ class QuicRServer
    * @param data                     : Opaque object payload
    * 
    */
-  void sendNamedObject(const QUICRContext& context_id,
-                       const QUICRName& quicr_name,
+  void sendNamedObject(const QUICRName& quicr_name,
                        uint8_t priority,
                        uint64_t best_before,
                        bool use_reliable_transport,
@@ -231,22 +216,22 @@ class QuicRServer
    /*
     * @brief Send a named QUICR media object fragment
     *
-    * @param context_id               : Opaque context identifier for correlation
     * @param quicr_name               : Identifies the QUICR Name for the object
     * @param priority                 : Identifies the relative priority of the current object
     * @param best_before              : TTL for the object to be useful for the application
     * @param use_reliable_transport   : Indicates the preference for the object's
     *                                   transport, if forwarded.
+    * @param offset                   : Current fragment offset
+    * @param is_last_fragment         : Indicates if the current fragment is the
+    *                                   last fragment
     * @param data                     : Opaque object fragment payload
-    * 
    */
-  void sendNamedFragment(const QUICRContext& context_id,
-                         const QUICRName& name,
+  void sendNamedFragment(const QUICRName& name,
                          uint8_t priority,
                          uint64_t best_before,
                          bool use_reliable_transport,
-                         uint16_t fragment_number,
-                         uint16_t num_total_fragments,
+                         uint64_t offset,
+                         bool is_last_fragment,
                          bytes&& data);
 };
     
