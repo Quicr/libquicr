fa4cb735353b46ba82c237ec5b129ebbfb2f2d50
Tim Evens <tievens@cisco.com>
2024-06-03 09:55:57 -0700

    Fix recv datagram handling with no data context

--- src/quicr_client_raw_session.cpp
+++ src/quicr_client_raw_session.cpp
@@ -935,7 +935,11 @@ ClientRawSession::handle(std::optional<uint64_t> stream_id,
 
         auto& context = subscribe_state[ns];
 
-        if (!data_ctx_id) {
+        if (!data_ctx_id && stream_id) {
+          /* NOTE:
+           * In picoquic there isn't a data_ctx_id because there isn't a
+           * stream context for datagram.
+           */
           transport->setStreamIdDataCtxId(context.transport_conn_id,
                                           context.transport_data_ctx_id,
                                           *stream_id);
