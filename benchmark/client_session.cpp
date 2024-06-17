153279b25b0a618b8c37417c5dbb635de9a4c128
trigaux <trigaux@cisco.com>
2024-06-17 11:35:58 -0400

    Better benchmark and multithreaded perf test.

--- benchmark/client_session.cpp
+++ benchmark/client_session.cpp
@@ -2,71 +2,93 @@
 
 #include <quicr/quicr_client.h>
 
+#include <atomic>
 #include <condition_variable>
 
-static auto logger = std::make_shared<cantina::Logger>("BENCH");
+static auto logger = std::make_shared<cantina::CustomLogger>([](auto, const std::string&, bool) {});
+
+std::condition_variable cv;
+std::mutex mutex;
+std::atomic_bool can_publish = false;
 
 struct BenchmarkPublishDelegate : public quicr::PublisherDelegate
 {
-  std::function<void()> benchmark_callback;
   void onPublishIntentResponse(const quicr::Namespace&,
                                const quicr::PublishIntentResult&) override
   {
-    benchmark_callback();
+    can_publish = true;
+    cv.notify_all();
   }
 };
 
-static void
-ClientSession_Publish(benchmark::State& state)
+class ClientFixture : public benchmark::Fixture
 {
-  quicr::RelayInfo info{
+public:
+  const quicr::RelayInfo info{
     .hostname = "relay.quicr.ctgpoc.com",
     .port = 33435,
     .proto = quicr::RelayInfo::Protocol::QUIC,
   };
-  qtransport::TransportConfig config{
+  const qtransport::TransportConfig config{
     .tls_cert_filename = nullptr,
     .tls_key_filename = nullptr,
     .use_reset_wait_strategy = false,
   };
 
-  quicr::Client client(info, "benchmark@cisco.com", 0, config, logger);
-  client.connect();
+  std::unique_ptr<quicr::Client> client;
 
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+  void SetUp(::benchmark::State&)
+  {
+    client = std::make_unique<quicr::Client>(
+      info, "benchmark@cisco.com", 0, config, logger);
+    client->connect();
+    client->publishIntent(
+      std::make_shared<BenchmarkPublishDelegate>(),
+      std::string_view{ "0x01020304050607080910111213141516/80" },
+      "",
+      "",
+      {},
+      quicr::TransportMode::ReliablePerTrack);
+  }
 
-  std::condition_variable cv;
-  std::mutex mutex;
-  auto delegate = std::make_shared<BenchmarkPublishDelegate>();
-  delegate->benchmark_callback = [&] {
-    const auto start_time =
-      std::chrono::time_point_cast<std::chrono::microseconds>(
-        std::chrono::steady_clock::now());
-    std::vector<qtransport::MethodTraceItem> trace;
-    for (auto _ : state) {
-      trace.push_back({ "qController:publishNamedObject", start_time });
-      state.ResumeTiming();
-      client.publishNamedObject(0x01020304050607080910111213141516_name,
-                                1,
-                                500,
-                                {},
-                                std::move(trace));
-      state.PauseTiming();
-      std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-    cv.notify_all();
-  };
+  void TearDown(::benchmark::State&)
+  {
+    client->disconnect();
+    client.reset();
+  }
+};
 
+BENCHMARK_DEFINE_F(ClientFixture, Publish)(benchmark::State& state)
+{
   std::unique_lock lock(mutex);
-  client.publishIntent(
-    delegate,
-    std::string_view{ "0x01020304050607080910111213141516/80" },
-    "",
-    "",
-    {},
-    quicr::TransportMode::ReliablePerTrack);
+  cv.wait(lock, [&] { return can_publish.load(); });
 
-  cv.wait(lock);
+  const auto start_time =
+    std::chrono::time_point_cast<std::chrono::microseconds>(
+      std::chrono::steady_clock::now());
+
+  constexpr quicr::Name name = 0x01020304050607080910111213141516_name;
+
+  for (auto _ : state) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    client->publishNamedObject(name,
+                               1,
+                               500,
+                               {},
+                               { {
+                                 "qController:publishNamedObject",
+                                 start_time,
+                               } });
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed_seconds =
+      std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+
+    state.SetIterationTime(elapsed_seconds.count());
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
 }
 
-BENCHMARK(ClientSession_Publish);
+BENCHMARK_REGISTER_F(ClientFixture, Publish)->UseManualTime();
