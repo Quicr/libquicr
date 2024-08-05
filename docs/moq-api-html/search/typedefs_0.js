5abee2f28f9a10a28f3c294b34d2cc505badd3b9
Tim Evens <tievens@cisco.com>
2024-08-05 10:05:50 -0700

    Update api docs

---
+++ docs/moq-api-html/search/typedefs_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sendobjfunction_0',['sendObjFunction',['../classquicr_1_1_mo_q_publish_track_handler.html#a7f92733deaba1f7575529785709cecd3',1,'quicr::MoQPublishTrackHandler']]]
+];
