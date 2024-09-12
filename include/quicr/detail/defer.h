c55d407241e858d3546035e03574bd799f25a2f4
trigaux <trigaux@cisco.com>
2024-09-12 16:26:22 -0400

    Cleanup.

--- include/quicr/detail/defer.h
+++ include/quicr/detail/defer.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2024 Cisco Systems
+// SPDX-License-Identifier: BSD-2-Clause
+
 #pragma once
 
 #include <functional>
@@ -5,18 +8,13 @@
 
 #define DEFER_CONCAT(a, b) DEFER_CONCAT_INNER(a, b)
 #define DEFER_CONCAT_INNER(a, b) a##b
-#define defer(n)                                                                                                       \
-    quicr::DeferType DEFER_CONCAT(defer_, __COUNTER__)                                                                 \
-    {                                                                                                                  \
-        [&] { n; }                                                                                                     \
-    }
+#define defer(n) quicr::DeferType DEFER_CONCAT(defer_, __COUNTER__)([&] { n; })
 
 namespace quicr {
-
     class DeferType
     {
       public:
-        explicit DeferType(std::function<void()> f)
+        explicit DeferType(std::function<void()>&& f)
           : func{ f }
         {
         }
