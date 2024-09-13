97af9249560b549e761623f1bbcc432c000d97b6
Tomas Rigaux <trigaux@cisco.com>
2024-09-13 10:56:18 -0400

    Performance and Scale Testing (#237)
    * Adding performance testing client.
    
    * Add some cleanup on end of test.
    
    * Updating with group size.
    
    * Adding defer mechanic.
    
    * Cleanup.

---
+++ include/quicr/detail/defer.h
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: Copyright (c) 2024 Cisco Systems
+// SPDX-License-Identifier: BSD-2-Clause
+
+#pragma once
+
+#include <functional>
+#include <iostream>
+
+#define DEFER_CONCAT(a, b) DEFER_CONCAT_INNER(a, b)
+#define DEFER_CONCAT_INNER(a, b) a##b
+#define defer(n) quicr::DeferType DEFER_CONCAT(defer_, __COUNTER__)([&] { n; })
+
+namespace quicr {
+    class DeferType
+    {
+      public:
+        explicit DeferType(std::function<void()>&& f)
+          : func{ f }
+        {
+        }
+        ~DeferType() { func(); }
+
+      private:
+        std::function<void()> func;
+    };
+}
