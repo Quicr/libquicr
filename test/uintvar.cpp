8b29926ae46970d0d533589c8570d89f96f7d842
trigaux <trigaux@cisco.com>
2024-09-18 17:47:26 -0400

    Adding uvarint tests.

--- test/uintvar.cpp
+++ test/uintvar.cpp
@@ -10,7 +10,7 @@ namespace var {
     constexpr uint64_t kValue8Byte = 0x123456789;
 }
 
-TEST_CASE("Encode/Decode UintVar")
+TEST_CASE("Encode/Decode UintVar Uint64")
 {
     CHECK_EQ(var::kValue1Byte, uint64_t(quicr::UintVar(var::kValue1Byte)));
     CHECK_EQ(var::kValue2Byte, uint64_t(quicr::UintVar(var::kValue2Byte)));
@@ -29,9 +29,14 @@ TEST_CASE("Length of UintVar")
     CHECK_EQ(2, quicr::UintVar(var::kValue2Byte).Bytes().size());
     CHECK_EQ(4, quicr::UintVar(var::kValue4Byte).Bytes().size());
     CHECK_EQ(8, quicr::UintVar(var::kValue8Byte).Bytes().size());
+
+    CHECK_EQ(1, quicr::UintVar::Size(quicr::UintVar(var::kValue1Byte).Bytes()[0]));
+    CHECK_EQ(2, quicr::UintVar::Size(quicr::UintVar(var::kValue2Byte).Bytes()[0]));
+    CHECK_EQ(4, quicr::UintVar::Size(quicr::UintVar(var::kValue4Byte).Bytes()[0]));
+    CHECK_EQ(8, quicr::UintVar::Size(quicr::UintVar(var::kValue8Byte).Bytes()[0]));
 }
 
-TEST_CASE("UintVar To/From Bytes")
+TEST_CASE("Encode/Decode UintVar Bytes")
 {
     CHECK_EQ(quicr::UintVar(quicr::UintVar(var::kValue1Byte).Bytes()), var::kValue1Byte);
     CHECK_EQ(quicr::UintVar(quicr::UintVar(var::kValue2Byte).Bytes()), var::kValue2Byte);
@@ -44,4 +49,5 @@ TEST_CASE("UintVar Invalid Construction")
     CHECK_THROWS(quicr::UintVar(std::numeric_limits<uint64_t>::max()));
     CHECK_THROWS(quicr::UintVar(std::vector<uint8_t>{}));
     CHECK_THROWS(quicr::UintVar(std::vector<uint8_t>(sizeof(uint64_t) + 1)));
+    CHECK_THROWS(quicr::UintVar(std::vector<uint8_t>{ 0xFF, 0xFF }));
 }
