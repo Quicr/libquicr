dbdf28427d072d65620634bba7a607d82422e3b5
Tim Evens <tievens@cisco.com>
2024-09-24 15:18:01 -0700

    ...

--- test/tick_service.cpp
+++ test/tick_service.cpp
@@ -12,13 +12,18 @@ TEST_CASE("TickService milliseconds")
 {
     constexpr int sleep_time_ms = 3;
 
+    auto prev_time = std::chrono::steady_clock::now();
     for (int i = 0; i < 10; i++) {
+        const auto& now = std::chrono::steady_clock::now();
+
         const auto start = var::tick_service.Milliseconds();
         std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time_ms));
-
         const auto delta = var::tick_service.Milliseconds() - start;
 
-        std::cerr << "delta: " << delta << std::endl;
+        const uint64_t local_delta = std::chrono::duration_cast<std::chrono::microseconds>(now - prev_time).count();
+        prev_time = now;
+
+        std::cerr << "local delta: " << local_delta << " delta: " << delta << std::endl;
 
         // Allow variance difference
         CHECK((delta >= sleep_time_ms - 2 && delta <= sleep_time_ms + 2));
@@ -29,11 +34,18 @@ TEST_CASE("TickService microseconds")
 {
     constexpr int sleep_time_us = 800;
 
+    auto prev_time = std::chrono::steady_clock::now();
     for (int i = 0; i < 10; i++) {
+        const auto& now = std::chrono::steady_clock::now();
+
         const auto start_us = var::tick_service.Microseconds();
         std::this_thread::sleep_for(std::chrono::microseconds(sleep_time_us));
-
         const auto delta = var::tick_service.Microseconds() - start_us;
+
+        const uint64_t local_delta = std::chrono::duration_cast<std::chrono::microseconds>(now - prev_time).count();
+        prev_time = now;
+
+        std::cerr << "local delta: " << local_delta << " delta: " << delta << std::endl;
 
         // Allow variance difference
         CHECK((delta >= sleep_time_us - 250 && delta <= sleep_time_us + 250));
