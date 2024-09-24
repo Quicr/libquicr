0f01e7c8cfcd36d747c6274af7d09a385ffd5299
Tim Evens <tievens@cisco.com>
2024-09-24 10:02:02 -0700

    Add tick service test

---
+++ test/tick_service.cpp
@@ -0,0 +1,39 @@
+#include <doctest/doctest.h>
+#include <iostream>
+
+#include <quicr/detail/time_queue.h>
+#include <unistd.h>
+
+namespace var {
+    auto tick_service = quicr::ThreadedTickService();
+}
+
+TEST_CASE("TickService milliseconds")
+{
+    constexpr int sleep_time_ms = 3;
+
+    for (int i = 0; i < 10; i++) {
+        const auto start_ms = var::tick_service.Milliseconds();
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time_ms));
+        const auto delta = var::tick_service.Milliseconds() - start_ms;
+
+        // Allow +-1ms difference
+        CHECK((delta >= sleep_time_ms - 1 && delta <= sleep_time_ms + 1));
+    }
+}
+
+TEST_CASE("TickService microseconds")
+{
+    constexpr int sleep_time_us = 800;
+
+    for (int i = 0; i < 10; i++) {
+
+        const auto start_us = var::tick_service.Microseconds();
+        std::this_thread::sleep_for(std::chrono::microseconds(sleep_time_us));
+
+        const auto delta = var::tick_service.Microseconds() - start_us;
+
+        // Allow +-200us difference
+        CHECK((delta >= sleep_time_us - 200 && delta <= sleep_time_us + 200));
+    }
+}
