0870e5608327dc18dd6feee10e3ac775f5a39b2e
Tim Evens <tievens@cisco.com>
2024-10-08 12:44:27 -0700

    FindMbedTLS esp_config.h

--- cmake/FindMbedTLS.cmake
+++ cmake/FindMbedTLS.cmake
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024 Cisco Systems
 # SPDX-License-Identifier: BSD-2-Clause
 
-find_path(MBEDTLS_INCLUDE_DIRS mbedtls/ssl.h)
+find_path(MBEDTLS_INCLUDE_DIRS mbedtls/ssl.h mbedtls/esp_config.h)
 
 find_library(MBEDTLS_LIBRARY mbedtls)
 find_library(MBEDX509_LIBRARY mbedx509)
