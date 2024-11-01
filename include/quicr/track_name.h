bfca029f8e1ec890681cc70ddd15d80561208160
trigaux <trigaux@cisco.com>
2024-11-01 09:17:44 -0400

    Redoing changes that got wiped out.

--- include/quicr/track_name.h
+++ include/quicr/track_name.h
@@ -12,6 +12,13 @@
 #include <vector>
 
 namespace quicr {
+    template<class T>
+    inline void hash_combine(std::size_t& seed, const T& v)
+    {
+        std::hash<T> hasher;
+        seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    }
+
     /**
      * @brief An N-tuple representation of a MOQ namespace.
      */
@@ -35,7 +42,9 @@ namespace quicr {
 
             std::size_t offset = 0;
             const auto add_entry = [&](auto&& e) {
-                entries_.emplace_back(Span{ bytes_ }.subspan(offset, e.size()));
+                const auto& entry = entries_.emplace_back(Span{ bytes_ }.subspan(offset, e.size()));
+                hash_.emplace_back(
+                  std::hash<std::string_view>{}({ reinterpret_cast<const char*>(entry.data()), entry.size() }));
                 offset += e.size();
             };
 
@@ -57,7 +66,9 @@ namespace quicr {
 
             std::size_t offset = 0;
             const auto add_entry = [&](auto&& e) {
-                entries_.emplace_back(Span{ bytes_ }.subspan(offset, e.size()));
+                const auto& entry = entries_.emplace_back(Span{ bytes_ }.subspan(offset, e.size()));
+                hash_.emplace_back(
+                  std::hash<std::string_view>{}({ reinterpret_cast<const char*>(entry.data()), entry.size() }));
                 offset += e.size();
             };
 
@@ -79,8 +90,11 @@ namespace quicr {
             std::size_t offset = 0;
             std::size_t i = 0;
             for (auto& entry : entries) {
-                entries_[i++] = Span{ bytes_ }.subspan(offset, entry.size());
+                entries_[i] = Span{ bytes_ }.subspan(offset, entry.size());
+                hash_.emplace_back(std::hash<std::string_view>{}(
+                  { reinterpret_cast<const char*>(entries_[i].data()), entries_[i].size() }));
                 offset += entry.size();
+                ++i;
             }
         }
 
@@ -98,14 +112,18 @@ namespace quicr {
             std::size_t offset = 0;
             std::size_t i = 0;
             for (auto& entry : entries) {
-                entries_[i++] = Span{ bytes_ }.subspan(offset, entry.size());
+                entries_[i] = Span{ bytes_ }.subspan(offset, entry.size());
+                hash_.emplace_back(std::hash<std::string_view>{}(
+                  { reinterpret_cast<const char*>(entries_[i].data()), entries_[i].size() }));
                 offset += entry.size();
+                ++i;
             }
         }
 
         TrackNamespace(const TrackNamespace& other)
           : bytes_(other.bytes_)
           , entries_(other.entries_)
+          , hash_(other.hash_)
         {
             std::size_t offset = 0;
             std::size_t i = 0;
@@ -118,6 +136,7 @@ namespace quicr {
         TrackNamespace(TrackNamespace&& other)
           : bytes_(std::move(other.bytes_))
           , entries_(std::move(other.entries_))
+          , hash_(std::move(other.hash_))
         {
             other.entries_.clear();
 
@@ -133,6 +152,7 @@ namespace quicr {
         {
             this->bytes_ = other.bytes_;
             this->entries_ = other.entries_;
+            this->hash_ = other.hash_;
 
             std::size_t offset = 0;
             std::size_t i = 0;
@@ -148,6 +168,7 @@ namespace quicr {
         {
             this->bytes_ = std::move(other.bytes_);
             this->entries_ = std::move(other.entries_);
+            this->hash_ = std::move(other.hash_);
 
             std::size_t offset = 0;
             std::size_t i = 0;
@@ -160,6 +181,7 @@ namespace quicr {
         }
 
         const std::vector<Span<const uint8_t>>& GetEntries() const noexcept { return entries_; }
+        const auto& GetHashes() const noexcept { return hash_; }
 
         // NOLINTBEGIN(readability-identifier-naming)
         auto begin() noexcept { return bytes_.begin(); }
@@ -192,19 +214,37 @@ namespace quicr {
 
         friend bool operator>=(const TrackNamespace& lhs, const TrackNamespace& rhs) noexcept { return !(lhs < rhs); }
 
-        bool Contains(const TrackNamespace& other) const noexcept
+        bool IsPrefixOf(const TrackNamespace& other) const noexcept
         {
             if (this->size() > other.size()) {
                 return false;
             }
 
-            auto other_view = Span{ other }.subspan(0, this->size());
-            return std::equal(this->begin(), this->end(), other_view.begin(), other_view.end());
+            return this->hash() == other.hash(this->hash_.size());
+        }
+
+        bool HasSamePrefix(const TrackNamespace& other) const noexcept
+        {
+            const std::size_t min_size = std::min(this->hash_.size(), other.hash_.size());
+            return this->hash(min_size) == other.hash(min_size);
+        }
+
+      private:
+        std::size_t hash(std::size_t offset = -1) const noexcept
+        {
+            std::size_t value = 0;
+            for (const auto& h : Span{ hash_ }.subspan(0, std::min(offset, hash_.size()))) {
+                hash_combine(value, h);
+            }
+            return value;
         }
 
       private:
         std::vector<uint8_t> bytes_;
         std::vector<Span<const uint8_t>> entries_;
+        std::vector<std::size_t> hash_;
+
+        friend struct std::hash<quicr::TrackNamespace>;
     };
 }
 
@@ -237,7 +277,7 @@ namespace quicr {
         TrackNamespaceHash track_namespace_hash = 0; // 64bit hash of namespace
         TrackNameHash track_name_hash = 0;           // 64bit hash of name
 
-        uint64_t track_fullname_hash = 0; // 62bit of namespace+name
+        size_t track_fullname_hash = 0; // 62bit of namespace+name
 
         constexpr TrackHash(const uint64_t name_space, const uint64_t name) noexcept
           : track_namespace_hash{ name_space }
@@ -251,7 +291,8 @@ namespace quicr {
           , track_name_hash{ std::hash<std::string_view>{}(
               { reinterpret_cast<const char*>(ftn.name.data()), ftn.name.size() }) }
         {
-            track_fullname_hash = (track_namespace_hash ^ (track_name_hash << 1)) << 1 >> 2;
+            hash_combine(track_fullname_hash, track_namespace_hash);
+            hash_combine(track_fullname_hash, track_name_hash);
         }
     };
 
