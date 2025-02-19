694632e42107293d0b52323015cdd354d68c5840
Rich Logan <rich@richlogan.co.uk>
2025-02-19 14:22:21 +0000

    Add support for short varint header extensions

--- test/moq_messages.cpp
+++ test/moq_messages.cpp
@@ -21,7 +21,7 @@ FromASCII(const std::string& ascii)
 const TrackNamespace kTrackNamespaceConf{ FromASCII("conf.example.com"), FromASCII("conf"), FromASCII("1") };
 const Bytes kTrackNameAliceVideo = FromASCII("alice/video");
 const UintVar kTrackAliasAliceVideo{ 0xA11CE };
-const Extensions kExampleExtensions = { { 0x1, { 0x1, 0x2 } } };
+const Extensions kExampleExtensions = { { 0x1, { 0x1, 0x2 } }, { 0x2, { 0xFF, 0, 0, 0, 0, 0, 0, 0 } } };
 const std::optional<Extensions> kOptionalExtensions = kExampleExtensions;
 
 template<typename T>
