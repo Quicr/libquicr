5ed462891fc43da8aa806b01d3cfe1a8e913406d
Tomas Rigaux <trigaux@cisco.com>
2025-09-19 10:06:18 -0400

    Update include/quicr/detail/data_storage.h

--- include/quicr/detail/data_storage.h
+++ include/quicr/detail/data_storage.h
@@ -111,7 +111,7 @@ namespace quicr {
             constexpr Iterator& operator--(int) noexcept
             {
                 Iterator temp(*this);
-                --temp;
+                --(*this);
                 return temp;
             }
 
