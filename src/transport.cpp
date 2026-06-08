aff330601a3b0c32d1699e9b07781c14f76cb5bb
Rich Logan <rich@richlogan.co.uk>
2026-06-08 13:40:39 +0100

    Reverse lookup for request ID

--- src/transport.cpp
+++ src/transport.cpp
@@ -778,6 +778,7 @@ namespace quicr {
 
         handler->SetDataContextId(quic_transport_->CreateDataContext(conn_id, true, 0, true, handler->GetRequestId()));
         quic_transport_->CreateStream(conn_id, handler->GetDataContextId().value(), 0);
+        conn_it->second.request_id_by_data_ctx[handler->GetDataContextId().value()] = handler->GetRequestId().value();
 
         SendSubscribeNamespace(conn_it->second,
                                handler->GetDataContextId().value(),
@@ -975,6 +976,8 @@ namespace quicr {
             track_handler->SetDataContextId(
               quic_transport_->CreateDataContext(conn_id, true, 0, true, track_handler->GetRequestId()));
             quic_transport_->CreateStream(conn_id, track_handler->GetDataContextId().value(), 0);
+            conn_it->second.request_id_by_data_ctx[track_handler->GetDataContextId().value()] =
+              track_handler->GetRequestId().value();
 
             SendSubscribe(conn_it->second,
                           track_handler->GetDataContextId().value(),
@@ -1187,6 +1190,10 @@ namespace quicr {
 
         const bool is_reset = flag == StreamClosedFlag::kReset;
 
+        if (const auto data_ctx_id = handler_it->second.handler->GetDataContextId()) {
+            conn_ctx.request_id_by_data_ctx.erase(*data_ctx_id);
+        }
+
         SPDLOG_LOGGER_INFO(logger_,
                            "Closing request handler conn_id: {} request_id: {} stream_id: {} reset: {}",
                            connection_handle,
@@ -1344,6 +1351,8 @@ namespace quicr {
         track_handler->SetDataContextId(
           quic_transport_->CreateDataContext(conn_id, true, 0, true, track_handler->GetRequestId()));
         quic_transport_->CreateStream(conn_id, track_handler->GetDataContextId().value(), 0);
+        conn_it->second.request_id_by_data_ctx[track_handler->GetDataContextId().value()] =
+          track_handler->GetRequestId().value();
 
         SendPublish(conn_it->second,
                     track_handler->GetDataContextId().value(),
@@ -1408,6 +1417,9 @@ namespace quicr {
             quic_transport_->CreateStream(conn_id, ns_handler->GetDataContextId().value(), 0);
 
             lock.lock();
+
+            conn_it->second.request_id_by_data_ctx[ns_handler->GetDataContextId().value()] =
+              ns_handler->GetRequestId().value();
 
             SendPublishNamespace(conn_it->second,
                                  ns_handler->GetDataContextId().value(),
